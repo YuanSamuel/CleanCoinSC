732ed98f0c28e8ebbf647e65b09e22ab8d00fa84
YuanSamuel <samuel6161@gmail.com>
2019-01-01 23:03:26 -0500

    Added some functions, made my comments according to Prasann's standards

--- app/src/main/java/com/stringcheesedevs/cleancoin/EmissionsCalc.java
+++ app/src/main/java/com/stringcheesedevs/cleancoin/EmissionsCalc.java
@@ -3,6 +3,8 @@ package com.stringcheesedevs.cleancoin;
 import android.content.Context;
 
 import com.stringcheesedevs.cleancoin.Models.Car;
+import com.stringcheesedevs.cleancoin.Models.Segment;
+import com.stringcheesedevs.cleancoin.Models.Trip;
 import com.stringcheesedevs.cleancoin.Persistence.CleanCoinDAO;
 
 import java.util.ArrayList;
@@ -15,8 +17,16 @@ public class EmissionsCalc
         datasource = new CleanCoinDAO(c);
     }
 
-    //@return how much fuel used given miles traveled
-    //@param milesTraveled in miles lol
+    /**
+     *
+     * TODO: Find change in efficiencies in trips, 
+     */
+
+    /**
+     *
+     * @param milesTraveled i wonder what this variable is
+     * @return how much fuel used given miles traveled
+     */
     public double getTripEmissions(double milesTraveled)
     {
         datasource.open();
@@ -25,42 +35,57 @@ public class EmissionsCalc
         return myCar.combinedmpg * milesTraveled;
     }
 
-    //@return percent efficiency compared to average car.
-    //@param mpg
-    //If returned number is negative, more efficient than normal car.
-    //If returned number is positive, less efficient than normal car.
+    /**
+     *
+     * @param mpg miles per gallon
+     * @return percent efficiency compared to the average car
+     * If returned number is negative, more efficient than normal car.
+     * If returned number is positive, less efficient than normal car.
+     */
     public double compareToAvg(double mpg)
     {
         return (mpg - 24.7) / 24.7 * 100;
     }
 
-    //@return percent efficiency compared to some other MPG.
-    //@param mpg of user, mpg of other
+    /**
+     *
+     * @param myMPG mpg of user
+     * @param otherMPG mpg of other
+     * @return percent efficiency compared to some other MPG.
+     */
     public double compareToOther(double myMPG, double otherMPG)
     {
         return (myMPG - otherMPG) / otherMPG * 100;
     }
 
-    //Return total emissions produced when cruising
-    //@param time in minutes
+    /**
+     *
+     * @param mpg miles per gallon
+     * @param timeInCruise time cruising in seconds
+     * @return total emissions produced when cruising
+     */
     public double getEmissionsWCruise(double mpg, double timeInCruise)
     {
-        return mpg * 107 / 100 * timeInCruise / 60;
+        return mpg * 107 / 100 * timeInCruise / 3600;
     }
 
-    //@return how much fuel
-    //@param speeds in mpg, time driven at each speed in minutes, if car is on highway or not
-    public double getTripEmissions(ArrayList<Double> speeds, ArrayList<Double> times, boolean onHighway)
+    /**
+     *
+     * @param speeds all speeds in mpg
+     * @param times times driven at each speed in seconds
+     * @param onHighway if car is on highway or not
+     * @return how much fuel used
+     */
+    public double getTripEmissions(ArrayList<Double> speeds, ArrayList<Double> times, ArrayList<Boolean> onHighway)
     {
         datasource.open();
         Car myCar = datasource.getUserCar();
         myCar.setMPG();
-        double usedAvg = onHighway ? myCar.highwayMPG : myCar.cityMPG;
         double emissions = 0;
         for (int i = 0; i < speeds.size(); i++)
         {
-            double time = times.get(i) / 60;
-            double wEfficiency = usedAvg;
+            double time = times.get(i) / 3600;
+            double wEfficiency = (onHighway.get(i)) ? myCar.highwayMPG : myCar.cityMPG;
             double speed = speeds.get(i);
             if (speed >= 60)
             {
@@ -81,20 +106,68 @@ public class EmissionsCalc
         return emissions;
     }
 
-    //@return fuel used
-    //@param mpg, times driven with each mpg in minutes
+    /**
+     *
+     * @param mpg miles per gallon
+     * @param times times driven with each mpg in seconds
+     * @return how much fuel used
+     */
     public double getTripEmissions(ArrayList<Double> mpg, ArrayList<Double> times)
     {
         double emissions = 0;
         for (int i = 0; i < mpg.size(); i++)
-            emissions += mpg.get(i) * times.get(i) / 60;
+            emissions += mpg.get(i) * times.get(i) / 3600;
         return emissions;
     }
 
-    //@return gallons of fuel used when idling
-    //@param mpg, time idling in minutes
+    /**
+     *
+     * @param mpg miles per gallon
+     * @param timeIdling time idling in seconds
+     * @return gallons of fuel used when idling
+     */
     public double gallonsWIdling(double mpg, double timeIdling)
     {
-        return 0.5 * timeIdling * 1 / mpg;
+        return 0.5 * timeIdling / 60 * 1 / mpg;
+    }
+
+    public double getTripEmissions(Trip trip)
+    {
+        ArrayList<Double> speeds = new ArrayList<Double>();
+        ArrayList<Double> times = new ArrayList<Double>();
+        ArrayList<Boolean> highway = new ArrayList<Boolean>();
+        for (int i = 0; i < trip.segments.size(); i++)
+        {
+            speeds.add(trip.segments.get(i).getSpeed());
+            times.add(trip.segments.get(i).time);
+            highway.add(trip.segments.get(i).onHighway);
+        }
+        return getTripEmissions(speeds, times, highway);
+    }
+
+    /**
+     *
+     * @param list
+     * @return list where the first element is the MPG of all previous trips
+     *          the second element is the MPG of the most recent trip
+     *          the third element is the percent efficiency of the most recent trip compared to all the other ones
+     */
+    public ArrayList<Double> getRecentEfficiency(ArrayList<Trip> list)
+    {
+        double dist = 0;
+        double emissions = 0;
+        for (int i = 0; i < list.size() - 1; i++)
+        {
+            dist += list.get(i).getTotalDistTraveled();
+            emissions += getTripEmissions(list.get(i));
+        }
+        dist /= 5280;
+        double beforeEfficiency = (emissions > 0) ? dist / emissions : 0;
+        double nowEfficiency = list.get(list.size() - 1).getTotalDistTraveled() / getTripEmissions(list.get(list.size() - 1));
+        ArrayList<Double> ret = new ArrayList<>();
+        ret.add(beforeEfficiency);
+        ret.add(nowEfficiency);
+        ret.add(compareToOther(nowEfficiency, beforeEfficiency));
+        return ret;
     }
 }
